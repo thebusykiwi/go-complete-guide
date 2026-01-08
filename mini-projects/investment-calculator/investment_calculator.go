196e822e8024dab3679cab06a671dbec3eb120da
vaardhanraju <vaardhanraju@gmail.com>
2026-01-08 17:56:09 +0000

    hard coded program

--- mini-projects/investment-calculator/investment_calculator.go
+++ mini-projects/investment-calculator/investment_calculator.go
@@ -1,7 +1,16 @@
 package main
 
-import "fmt"
+import (
+	"math"
+	"fmt"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	var investmentAmount = 1000
+	var expectedReturnRate = 5.5
+	var years = 10
+	
+	var futureValue = float64(investmentAmount) * math.Pow(1 + expectedReturnRate / 100, float64(years))
+
+	fmt.Println(futureValue)
 }
